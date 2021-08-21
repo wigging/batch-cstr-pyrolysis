0f3b36d9d1ad75e2d04f3576c0a2c6bb05e66c93
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2021-08-21 00:00:22 -0400

    Add CSTR model for all feedstocks

---
+++ src/cstr_all.py
@@ -0,0 +1,298 @@
+"""
+Run a series of CSTR models for all FCIC feedstocks using the Debiagi 2018
+biomass pyrolysis kinetics. All reactions and chemical species are defined in
+Cantera `cti` files where `debiagi_sw.cti` is for softwood reactions,
+`debiagi_hw.cti` is for hardwood reactions, and `debiagi_gr.cti` is for grass
+reactions.
+
+Note
+----
+The `debiagi_sw.cti` uses the original reaction rates from Debiagi 2018 while
+`debiagi_sw_metan2.cti` uses modified rates for the metaplastic reactions
+such that b=1 instead of b=0 where b is the temperature exponent Tᵇ.
+
+Reference
+---------
+P. Debiagi, G. Gentile, A. Cuoci, A. Frassoldati, E. Ranzi, and T. Faravelli.
+A predictive model of biochar formation and characterization. Journal of
+Analytical and Applied Pyrolysis, vol. 134, pp. 326-335, 2018.
+"""
+
+import cantera as ct
+import chemics as cm
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from feedstock import Feedstock
+from objfunc import objfunc
+from scipy.optimize import minimize
+
+# Disable warnings about discontinuity at polynomial mid-point in thermo data.
+# Remove this line to show the warnings.
+ct.suppress_thermo_warnings()
+
+# Parameters
+# ----------------------------------------------------------------------------
+
+diam = 0.0525    # inner reactor diameter [m], 5.25 cm
+length = 0.4318  # total reactor length or height [m], 43.18 cm
+temp = 773.15    # reactor temperature [K]
+pabs = 101_325   # reactor absolute pressure [Pa]
+tau = 20         # residence time [s]
+
+ghr_bio = 420    # biomass inlet feedrate [g/hr]
+slm_n2 = 14      # inlet nitrogen gas flowrate [SLM]
+
+n_cstrs = 1000                      # number of CSTRs in series
+energy = 'off'                      # reactor energy
+cti = 'data/debiagi_sw_metan2.cti'  # Cantera input file
+
+# Feedstocks
+# ----------------------------------------------------------------------------
+
+with open("data/feedstocks.json") as json_file:
+    fdata = json.load(json_file)
+
+feedstocks = [Feedstock(fd) for fd in fdata]
+
+# Reactor inputs
+# ----------------------------------------------------------------------------
+
+# Convert biomass feedrate from g/hr to kg/s
+mf_bio = ghr_bio / 1000 / 3600
+
+# Convert nitrogen gas flowrate from SLM to kg/s
+# 1 m³/s = 60,000 liter/minute
+# N₂ gas molecular weight = 28 g/mol
+# N₂ gas density at STP = 1.2506 kg/m³
+lpm_n2 = cm.slm_to_lpm(slm_n2, pabs / 1000, temp)
+rhog_n2 = cm.rhog(28, 101325, 773.15)
+mf_n2 = lpm_n2 / 60_000 * rhog_n2
+
+# Mass fraction of nitrogen gas into reactor
+y0_n2 = mf_n2 / (mf_n2 + mf_bio)
+
+# Cantera CSTR model
+# ----------------------------------------------------------------------------
+
+# Store results for each feedstock
+names = []
+nf = len(feedstocks)
+yf_gases = np.zeros(nf)
+yf_liquids = np.zeros(nf)
+yf_solids = np.zeros(nf)
+yf_metas = np.zeros(nf)
+exp_gases = np.zeros(nf)
+exp_liquids = np.zeros(nf)
+exp_solids = np.zeros(nf)
+exp_ash = np.zeros(nf)
+
+# Run CSTR reactor model for each feedstock
+for i, f in enumerate(feedstocks):
+    print('Run', i, f.name)
+
+    # Get feedstock ultimate analysis data, and chemical analysis data
+    ult_daf = f.calc_ult_daf()
+    ult_cho = f.calc_ult_cho(ult_daf)
+    chem_daf = f.calc_chem_daf()
+    chem_bc = f.calc_chem_bc(chem_daf) / 100
+
+    # C and H mass fractions from ultimate analysis data
+    yc = ult_cho[0] / 100
+    yh = ult_cho[1] / 100
+
+    # Determine optimized splitting parameters using default values for `x0`
+    # where each parameter is bound within 0 to 1
+    x0 = [0.6, 0.8, 0.8, 1, 1]
+    bnds = ((0, 1), (0, 1), (0, 1), (0, 1), (0, 1))
+    res = minimize(objfunc, x0, args=(yc, yh, chem_bc), method='L-BFGS-B', bounds=bnds)
+
+    # Calculate biomass composition as dry ash-free basis (daf)
+    bc = cm.biocomp(yc, yh, alpha=res.x[0], beta=res.x[1], gamma=res.x[2], delta=res.x[3], epsilon=res.x[4])
+    cell, hemi, ligc, ligh, ligo, tann, tgl = bc['y_daf']
+
+    # All mass fractions for reactor input
+    y_all = {
+        'N2': y0_n2,
+        'CELL': cell,
+        'GMSW': hemi,
+        'LIGC': ligc,
+        'LIGH': ligh,
+        'LIGO': ligo,
+        'TANN': tann,
+        'TGL': tgl
+    }
+
+    # Setup gas phase
+    gas = ct.Solution(cti)
+    gas.TPY = temp, pabs, y_all
+
+    # Create a stirred tank reactor (CSTR)
+    dz = length / n_cstrs
+    area = np.pi * (diam**2)
+    vol = area * dz
+    cstr = ct.IdealGasConstPressureReactor(gas, energy=energy, volume=vol)
+
+    # Reservoirs for the inlet and outlet of each CSTR
+    inlet = ct.Reservoir(gas)
+    outlet = ct.Reservoir(gas)
+
+    # Mass flow rate into the CSTR
+    mf = ct.MassFlowController(upstream=inlet, downstream=cstr, mdot=cstr.mass / tau)
+
+    # Determine pressure in the CSTR
+    ct.PressureController(upstream=cstr, downstream=outlet, master=mf, K=1e-5)
+
+    # Create a reactor network for performing the simulation
+    sim = ct.ReactorNet([cstr])
+
+    # Store the results for each CSTR
+    states = ct.SolutionArray(cstr.thermo)
+    states.append(cstr.thermo.state)
+
+    for n in range(n_cstrs):
+        gas.TPY = cstr.thermo.TPY
+        inlet.syncState()
+        sim.reinitialize()
+        sim.advance_to_steady_state()
+        states.append(cstr.thermo.state)
+
+    # Species representing each phase
+    gases = ('C2H4', 'C2H6', 'CH2O', 'CH4', 'CO', 'CO2', 'H2', 'N2')
+
+    liquids = (
+        'C2H3CHO', 'C2H5CHO', 'C2H5OH', 'C5H8O4', 'C6H10O5', 'C6H5OCH3', 'C6H5OH',
+        'C6H6O3', 'C24H28O4', 'CH2OHCH2CHO', 'CH2OHCHO', 'CH3CHO', 'CH3CO2H',
+        'CH3OH', 'CHOCHO', 'CRESOL', 'FURFURAL', 'H2O', 'HCOOH', 'MLINO', 'U2ME12',
+        'VANILLIN'
+    )
+
+    solids = (
+        'CELL', 'CELLA', 'GMSW', 'HCE1', 'HCE2', 'ITANN', 'LIG', 'LIGC', 'LIGCC',
+        'LIGH', 'LIGO', 'LIGOH', 'TANN', 'TGL', 'CHAR', 'ACQUA'
+    )
+
+    metaplastics = (
+        'GCH2O', 'GCO2', 'GCO', 'GCH3OH', 'GCH4', 'GC2H4', 'GC6H5OH', 'GCOH2',
+        'GH2', 'GC2H6'
+    )
+
+    # Mass fractions including nitrogen and biomass (N₂ basis)
+    y_n2 = states('N2').Y[:, 0]
+    y_gas = states(*gases).Y.sum(axis=1)
+    y_liquid = states(*liquids).Y.sum(axis=1)
+    y_solid = states(*solids).Y.sum(axis=1)
+    y_meta = states(*metaplastics).Y.sum(axis=1)
+
+    # Mass fractions from only biomass (no nitrogen gas, N₂ free basis)
+    sum_bio = y_gas + y_liquid + y_solid + y_meta - y_n2
+    y_gas_bio = (y_gas - y_n2) / sum_bio
+    y_liquid_bio = y_liquid / sum_bio
+    y_solid_bio = y_solid / sum_bio
+    y_meta_bio = y_meta / sum_bio
+
+    # Store exit yields for feedstock
+    names.append(f.name)
+    yf_gases[i] = y_gas_bio[-1]
+    yf_liquids[i] = y_liquid_bio[-1]
+    yf_solids[i] = y_solid_bio[-1]
+    yf_metas[i] = y_meta_bio[-1]
+
+    # Experiment lumped yields
+    exp_yields, norm_yields = f.calc_yields()
+    exp_lumps, _ = f.calc_lump_yields(exp_yields, norm_yields)
+    exp_gases[i] = exp_lumps[0]
+    exp_liquids[i] = exp_lumps[1]
+    exp_solids[i] = exp_lumps[2]
+    exp_ash[i] = f.prox[2]
+
+# Print
+# ----------------------------------------------------------------------------
+
+print(f'\n{" CSTR model ":*^70}\n')
+
+print(
+    'Parameters for reactor model\n'
+    f'diam       {diam} m\n'
+    f'length     {length} m\n'
+    f'temp       {temp} K\n'
+    f'pabs       {pabs:,} Pa\n'
+    f'tau        {tau} s\n'
+    f'n_cstrs    {n_cstrs}\n'
+    f'energy     {energy}\n'
+    f'cti file   {cti}\n'
+)
+
+
+# Plot
+# ----------------------------------------------------------------------------
+
+def style_barh(ax):
+    ax.invert_yaxis()
+    ax.set_axisbelow(True)
+    ax.set_frame_on(False)
+    ax.tick_params(color='0.8')
+    ax.xaxis.grid(True, color='0.8')
+
+
+y = np.arange(nf)
+
+_, ax = plt.subplots(tight_layout=True, figsize=(9, 4.8))
+b1 = ax.barh(y, yf_gases * 100, color='C6', label='Gases')
+b2 = ax.barh(y, yf_liquids * 100, left=yf_gases * 100, color='C4', label='Liquids')
+b3 = ax.barh(y, yf_solids * 100, left=(yf_gases + yf_liquids) * 100, color='C2', label='Solids')
+b4 = ax.barh(y, yf_metas * 100, left=(yf_gases + yf_liquids + yf_solids) * 100, color='C1', label='Metaplastics')
+ax.bar_label(b1, label_type='center', fmt='%.1f')
+ax.bar_label(b2, label_type='center', fmt='%.1f')
+ax.bar_label(b3, label_type='center', fmt='%.1f')
+ax.bar_label(b4, label_type='center', fmt='%.1f')
+ax.legend(bbox_to_anchor=[0.5, 1.02], loc='center', ncol=4, frameon=False)
+ax.set_yticks(y)
+ax.set_yticklabels(names)
+ax.set_xlabel('Final yield [wt. %]')
+style_barh(ax)
+
+# ---
+
+h = 0.4
+yf_gases = yf_gases * 100
+yf_liquids = yf_liquids * 100
+yf_solids = yf_solids * 100
+yf_metas = yf_metas * 100
+yf_solidmeta = yf_solids + yf_metas
+
+_, ax = plt.subplots(tight_layout=True, figsize=(9, 8))
+
+b1 = ax.barh(y, yf_gases, edgecolor='k', height=h, color='C6', label='Gases')
+b2 = ax.barh(y, yf_liquids, edgecolor='k', left=yf_gases, height=h, color='C4', label='Liquids')
+b3 = ax.barh(y, yf_solidmeta, edgecolor='k', left=yf_gases + yf_liquids, height=h, color='C2', label='Solids')
+
+e1 = ax.barh(y + h, exp_gases, edgecolor='k', height=h, color='C6')
+e2 = ax.barh(y + h, exp_liquids, edgecolor='k', height=h, left=exp_gases, color='C4')
+e3 = ax.barh(y + h, exp_solids, edgecolor='k', height=h, left=exp_gases + exp_liquids, color='C2')
+
+ax.bar_label(b1, label_type='center', fmt='%.1f')
+ax.bar_label(e1, label_type='center', fmt='%.1f')
+ax.bar_label(b2, label_type='center', fmt='%.1f')
+ax.bar_label(e2, label_type='center', fmt='%.1f')
+ax.bar_label(b3, label_type='center', fmt='%.1f')
+ax.bar_label(e3, label_type='center', fmt='%.1f')
+
+ax.legend(bbox_to_anchor=[0.5, 1.02], loc='center', ncol=3, frameon=False)
+ax.set_yticks(y + h / 2)
+ax.set_yticklabels(names)
+ax.set_xlabel('Final yield [wt. %]')
+style_barh(ax)
+
+# ---
+
+z = np.polyfit(exp_ash, yf_liquids, 1)
+p = np.poly1d(z)
+
+_, ax = plt.subplots()
+ax.plot(exp_ash, yf_liquids, 'o')
+ax.plot(exp_ash, p(exp_ash))
+ax.set_xlabel('Ash [wt. %]')
+ax.set_ylabel('Liquids [wt. %]')
+
+plt.show()
